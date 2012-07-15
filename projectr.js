3a7c0b7f6b6d199267dabc01b1d3592dfba48b92
Mike Gardiner <conversationing@gmail.com>
2012-07-15 10:44:22 +0800

    Libify!

--- projectr.js
+++ projectr.js
@@ -1,9 +1,2 @@
-var lat = process.env.LAT;
-var lng = process.env.LNG;
-
-var twitter_stream = require('./twitter_stream_source').twitter_stream_source;
-twitter_stream.track_terms(process.env.FIREHOSE_TERMS);
-twitter_stream.track_current_user();
-
-var instagram = require('./instagram_source').instagram_source;
-instagram.track(lat, lng);
+var streamers = require('./lib/streamers').streamers;
+streamers.start();
