a6c35689e440c87990be0bde65cb06ab78c06f81
Mike Gardiner <conversationing@gmail.com>
2012-07-16 20:41:38 +0800

    Re-include the polling events call for reference.

--- events-api/public/javascripts/main-index.js
+++ events-api/public/javascripts/main-index.js
@@ -9,6 +9,16 @@ requirejs.config({
 });
 
 require(['jquery', 'moment', 'http://localhost:5001/socket.io/socket.io.js'], function($, moment, io) {
+
+//  $.get("/events?from=" + start_time.unix() + "&to=" + now_time.unix(), function(tweets) {
+//    $.each(tweets, function(_, tweet) {
+//      $("#output").append("<p>" + tweet.username + ": " + tweet.text + "</p>");
+//      if(tweet.image) {
+//        $("#output").append("<p><img src='" + tweet.image + "' /></p>")
+//      }
+//    });
+//  });
+
   var socket = io.connect('http://localhost:5001');
   socket.on('evt', function(tweets) {
     $.each(tweets, function(_, tweet) {
@@ -18,4 +28,5 @@ require(['jquery', 'moment', 'http://localhost:5001/socket.io/socket.io.js'], fu
       }
     });
   });
+
 });
