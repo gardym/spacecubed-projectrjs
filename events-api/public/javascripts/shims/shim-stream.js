4b89a7c85649e668be4408c51a9ccf45ac637cc7
Mike Gardiner <conversationing@gmail.com>
2012-07-18 19:03:35 +0800

    Kick off the ticker.

--- events-api/public/javascripts/shims/shim-stream.js
+++ events-api/public/javascripts/shims/shim-stream.js
@@ -3,11 +3,12 @@ define(['map'], function(map) {
 
     return {
       username: 'gardym',
-      text: "Here's my test comment",
-      coordinates: {
-        lat: map.origin_lat - (Math.random() * map.lat_width),
-        lng: map.origin_lng + (Math.random() * map.lng_height)
-      }
+      text: "I can't believe I ate the whole thing #omg",
+      profile_image: "http://a0.twimg.com/profile_images/1057847588/3405116394_c181846851_m_normal.jpg"
+      //coordinates: {
+        //lat: map.origin_lat - (Math.random() * map.lat_width),
+        //lng: map.origin_lng + (Math.random() * map.lng_height)
+      //}
     };
   };
 
