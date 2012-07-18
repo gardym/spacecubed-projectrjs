9e2abf4f04948335078bff562a38f2cb1989f081
Ray Grasso <ray.grasso@gmail.com>
2012-07-18 18:27:03 +0800

    Restyle boxes

--- events-api/public/javascripts/shims/shim-stream.js
+++ events-api/public/javascripts/shims/shim-stream.js
@@ -3,7 +3,8 @@ define(['map'], function(map) {
 
     return {
       username: 'gardym',
-      text: "Here's my test comment",
+      text: "All of the donkeys ride the rainbows of my life with dark sunglasses on their brains",
+      profile_image : "http://a0.twimg.com/profile_images/1057847588/3405116394_c181846851_m_normal.jpg",
       coordinates: {
         lat: map.origin_lat - (Math.random() * map.lat_width),
         lng: map.origin_lng + (Math.random() * map.lng_height)
