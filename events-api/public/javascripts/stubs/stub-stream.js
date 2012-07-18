0af454e2a4771c29486f3510bc9bbe456635b439
Mike Gardiner <conversationing@gmail.com>
2012-07-18 22:06:50 +0800

    Add a simple ticker for events.

--- events-api/public/javascripts/stubs/stub-stream.js
+++ events-api/public/javascripts/stubs/stub-stream.js
@@ -1,21 +1,25 @@
 define(['map'], function(map) {
   var stub_event = function() {
 
+    if(Math.random() > 0.5) {
+      var coordinates = {
+        lat: map.origin_lat - (Math.random() * map.lat_width),
+        lng: map.origin_lng + (Math.random() * map.lng_height)
+      };
+    }
+
     return {
       username: 'gardym',
       text: "All of the donkeys ride the rainbows of my life with dark sunglasses on their brains",
-      profile_image : "http://a0.twimg.com/profile_images/1057847588/3405116394_c181846851_m_normal.jpg",
-      coordinates: {
-        lat: map.origin_lat - (Math.random() * map.lat_width),
-        lng: map.origin_lng + (Math.random() * map.lng_height)
-      }
+      profile_image: "http://a0.twimg.com/profile_images/1057847588/3405116394_c181846851_m_normal.jpg",
+      coordinates: coordinates
     };
   };
 
   var on = function(fn) {
     setInterval(function() {
       fn([stub_event()]);
-    }, 3000);
+    }, 1500);
   };
 
   return { on: on };
--- events-api/public/javascripts/ticker.js
+++ events-api/public/javascripts/ticker.js
@@ -1,12 +1,55 @@
-define(function() {
+define(["jquery"], function($) {
 
   var events = [];
   var addEvent = function(evt) {
     events.push(evt);
   };
 
+  var moveDown = function(event) {
+  };
+
+  var drawVisible = function(event) {
+    var event_container = $('#template').clone();
+    event_container.removeAttr('id');
+    event_container.removeClass('invisible');
+
+    event_container.find('.profile-image').attr('src', event.profile_image);
+    event_container.find('.text').text(event.text);
+    event_container.find('.author').text(event.username);
+
+    event_container.css('position', 'absolute');
+    event_container.css('top', '-44px');
+    event_container.css('left', '1050px');
+
+    $('#map').append(event_container);
+
+    return event_container;
+  };
+
+  var moveDown = function(visible) {
+    setTimeout(function() {
+      var top = visible.position().top;
+      var height = visible.height();
+      visible.css('top', top + height + 'px');
+      setTimeout(function() {
+        if(visible.position().top > ($("#map").height() - (4 * visible.height()))) {
+          visible.css('opacity', '0');
+        }
+      }, 1);
+    }, 1);
+  };
+
+  var visibles = [];
   var draw = function() {
-    console.log(events);
+    var newEvent = events.shift();
+    if(newEvent) {
+      var visible = drawVisible(newEvent);
+      visibles.push(visible);
+
+      $.each(visibles, function(_, event) {
+        moveDown(event);
+      });
+    }
   };
 
   var start = function(interval) {
