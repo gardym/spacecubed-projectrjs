83a1cc234213423c4c60a63d2a8484f76e59d88c
Mike Gardiner <conversationing@gmail.com>
2012-07-24 19:39:34 +1000

    Use some real(?) stub data.

--- events-api/public/javascripts/stubs/stub-stream.js
+++ events-api/public/javascripts/stubs/stub-stream.js
@@ -9,8 +9,8 @@ define(['map'], function(map) {
     }
 
     return {
-      username: 'gardym',
-      text: "All of the donkeys ride the rainbows of my life with dark sunglasses on their brains",
+      username: 'sii_wa',
+      text: "If you are at #fground2012 today don't forget to see @TomTolchard at 3.30 talk about Understanding Social Enterprise",
       profile_image: "http://a0.twimg.com/profile_images/1057847588/3405116394_c181846851_m_normal.jpg",
       coordinates: coordinates
     };
