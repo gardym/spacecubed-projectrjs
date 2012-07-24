0198022e54a443a5594453453ccf1ed6f98b1ef0
Mike Gardiner <conversationing@gmail.com>
2012-07-24 20:33:38 +1000

    Divorce featured events from their ticking cousins.

--- events-api/public/javascripts/promoter.js
+++ events-api/public/javascripts/promoter.js
@@ -4,24 +4,6 @@ define(['jquery'], function($) {
     events.push(evt);
   };
 
-  var drawVisible = function(event) {
-    var event_container = $('#template').clone();
-    event_container.removeAttr('id');
-    event_container.removeClass('invisible');
-
-    event_container.find('.profile-image').attr('src', event.profile_image);
-    event_container.find('.text').text(event.text);
-    event_container.find('.author').text(event.username);
-
-    event_container.css('position', 'absolute');
-    event_container.css('top', '-44px');
-    event_container.css('left', '1050px');
-
-    $('#map').append(event_container);
-
-    return event_container;
-  };
-
   var feature;
   var draw = function() {
 
@@ -39,18 +21,25 @@ define(['jquery'], function($) {
       }, 1);
     }
 
-    var event = drawVisible(newEvent);
-    feature = event;
-    event.addClass('feature');
-    event.find('.text').html('&#147;' + event.find('.text').text() +'&#148;');
-    event.css('fontSize', '2em');
+    var event_container = $('#template').clone();
+    feature = event_container;
+
+    $('#map').append(event_container);
+    event_container.removeAttr('id').removeClass('invisible').addClass('feature');
+
+    event_container.find('.profile-image').hide();
+    event_container.find('.text').html('&#147;' + newEvent.text +'&#148;');
+    event_container.find('.author').text(newEvent.username);
+
+
     var random_offset = Math.floor(Math.random() * 15);
-    event.css('top', random_offset + 'px');
-    event.css('left', random_offset + 'px');
-    event.css('opacity', '0');
+    event_container.css('position', 'absolute');
+    event_container.css('top', random_offset + 'px');
+    event_container.css('left', random_offset + 'px');
+
     setTimeout(function() {
-      event.css('opacity', '0.8');
-    }, 1);
+      event_container.css('opacity', '0.8');
+    }, 5);
 
   };
 
