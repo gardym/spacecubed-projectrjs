8147a6ba3637d8d51a2b874183f448b188ede066
Mike Gardiner <conversationing@gmail.com>
2012-07-24 22:30:45 +0800

    Use MongoLabs on Heroku for querying events.

--- events-api/routes/index.js
+++ events-api/routes/index.js
@@ -17,11 +17,15 @@ exports.client = function(req, res) {
  * GET events
  */
 
+var moment = require('moment'),
+    mongo  = require('mongodb');
+
+var db_options = {
+  'auto_reconnect': true,
+  'poolSize': 10
+};
+
 exports.events = function(req, res){
-
-  var moment = require('moment'),
-      mongo  = require('mongodb');
-
   var from = moment.unix(req.query.from),
       to   = moment.unix(req.query.to);
 
@@ -31,9 +35,7 @@ exports.events = function(req, res){
     recorded_at: { '$gte': from.toDate(), '$lt' : to.toDate() }
   };
 
-  var db = new mongo.Db('projectrjs', new mongo.Server('localhost', 27017));
-
-  db.open(function() {
+  var search = function(err, db) {
     db.collection('events', function(err, events_collection) {
       events_collection.find(query, function(err, cursor) {
         cursor.toArray(function(err, items) {
@@ -43,5 +45,15 @@ exports.events = function(req, res){
         });
       });
     });
-  });
+  };
+
+  if(process.env.MONGOLAB_URI) {
+    var db_connection = new mongo.Db.connect(process.env.MONGOLAB_URI,
+                                             db_options,
+                                             search);
+  } else {
+    var db_connection = new mongo.Db('projectrjs',
+                        new mongo.Server('localhost', 27017, db_options));
+    db_connection.open(search);
+  }
 };
