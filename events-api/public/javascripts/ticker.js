5dfccbe4d69cde853bf089431ae4d51c14ad2bac
Mike Gardiner <conversationing@gmail.com>
2012-07-25 20:22:26 +0800

    Move the ticker at a rate that never overlaps.

--- events-api/public/javascripts/ticker.js
+++ events-api/public/javascripts/ticker.js
@@ -5,20 +5,27 @@ define(["jquery"], function($) {
     events.push(evt);
   };
 
+  var distance;
   var drawVisible = function(event) {
     var event_container = $('#template').clone();
-    event_container.removeAttr('id');
-    event_container.removeClass('invisible');
+    event_container.removeAttr('id').removeClass('invisible');
 
     event_container.find('.profile-image').attr('src', event.profile_image);
     event_container.find('.text').text(event.text);
     event_container.find('.author').text(event.username);
 
     event_container.css('position', 'absolute');
-    event_container.css('top', '-44px');
+    event_container.css('top', '-500px');
     event_container.css('left', '1050px');
 
     $('#map').append(event_container);
+    var offset = '-' + event_container.height().toString() + 'px';
+    event_container.remove();
+
+    event_container.css('top', offset);
+    $('#map').append(event_container);
+
+    distance = event_container.height();
 
     return event_container;
   };
@@ -26,8 +33,7 @@ define(["jquery"], function($) {
   var moveDown = function(visible) {
     setTimeout(function() {
       var top = visible.position().top;
-      var height = 75;
-      visible.css('top', top + height + 'px');
+      visible.css('top', top + distance + 'px');
       setTimeout(function() {
         if(visible.position().top > ($("#map").height() - 210)) {
           visible.css('opacity', '0');
