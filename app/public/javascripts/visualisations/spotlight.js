5fb07abd3f7d410d0814df55ab71ea7686f49b2a
Mike Gardiner <conversationing@gmail.com>
2012-08-14 20:06:55 +0800

    Use a deferred loaded for figuring out when Instagram images have finished loading.

--- app/public/javascripts/visualisations/spotlight.js
+++ app/public/javascripts/visualisations/spotlight.js
@@ -1,26 +1,41 @@
-define(['jquery', 'visualisations/animations/circular'], function($, CircularAnimation) {
+define(['jquery', 'lib/jquery.imagesloaded', 'visualisations/animations/circular'],
+    function($, jQueryImagesLoaded, CircularAnimation) {
+
   return function(event) {
     var animation;
     var imageBox;
+    var image;
 
-    var displayImageBox = function(event) {
-      imageBox = $('<div><img src="' + event.image + '" style="width: 100%; height: 100%" /></div>');
+    var preloadImage = function(event) {
+      image = $('<img src="' + event.image + '" style="width: 100%; height: 100%" />');
+      return image.imagesLoaded();
+    };
+
+    var displayImageBox = function(event, imageLoadedDeferred) {
+      imageBox = $('<div></div>');
+      imageBox.hide();
+      imageBox.append(image);
       $('#map').append(imageBox);
 
       var offset = 120;
       imageBox.css('top', (event.xy.y - (offset / 2)) + 'px');
       imageBox.css('left', (event.xy.x + offset) + 'px');
       imageBox.css('position', 'absolute');
-      imageBox.css('width', '120px');
-      imageBox.css('height', '120px');
+      imageBox.css('width', '240px');
+      imageBox.css('height', '240px');
+
+      imageLoadedDeferred.always(function() {
+        imageBox.show();
+      });
     };
 
     this.shineOnYouCrazyDiamond = function() {
+      var imageLoadedDeferred = preloadImage(event);
       animation = new CircularAnimation(event.xy);
       animation.startAnimation(function() {
         // Show our details (instagram and tweet)
         if(event.image) {
-          displayImageBox(event);
+          displayImageBox(event, imageLoadedDeferred);
         } else {
           displayTextBox(event);
         }
