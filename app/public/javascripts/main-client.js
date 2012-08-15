e2bf308be6e11b888b00e2414c1eb8c654f3e753
Ray Grasso <ray.grasso@gmail.com>
2012-08-15 19:24:13 +0800

    Wire in a fake and live stream

--- app/public/javascripts/main-client.js
+++ app/public/javascripts/main-client.js
@@ -4,41 +4,25 @@ var config = {
   }
 }
 
-// Use a stub if we are developing locally
-if (window.location.hostname == 'localhost') {
+if (window.location.search.indexOf('data=fake') != -1) {
   config.map = {
-    '*': { 'data/stream': 'stubs/stub-stream' }
+    '*': { 'data/live_event_stream': 'data/fake_event_stream' }
   }
 }
 
 requirejs.config(config);
 
-require(['jquery', 'data/stream', 'visualisations/map', 'data/live_events', 'visualisations/ticker', 'visualisations/promoter'],
-        function($, stream, map, live_events, ticker, promoter) {
+require(['jquery', 'data/stream', 'visualisations/map', 'data/live_event_stream', 'visualisations/ticker', 'visualisations/promoter'],
+        function($, stream, map, event_stream, ticker, promoter) {
 
   $(function(){
-    var locatableEvents = live_events.events.filter(function(event) {
-      return event.coordinates;
+    event_stream.eventsToDate(function(eventsToDate) {
+
+      var locatableEvents = eventsToDate.filter(function(event) {
+        return event.coordinates;
+      });
+
+      map.create(locatableEvents);
     });
-    map.create(locatableEvents);
-//    map.create([locatableEvents.pop()]);
-
-    //ticker.start(3000);
-    //promoter.start(3000);
   });
-
-  //stream.on(function(events) {
-    //$.each(events, function(_, event) {
-      //if (event.coordinates) {
-        //map.addEvent(event);
-      //} else {
-        //// Define some logic to work out what should be featured
-        //// ... or leave it random.
-        //if(Math.random() < 0.1) {
-          //promoter.addEvent(event);
-        //}
-        //ticker.addEvent(event);
-      //}
-    //});
-  //});
 });
