1689d299e009aa320f028258377363cc41de9922
Mike Gardiner <conversationing@gmail.com>
2012-08-18 16:01:33 +0800

    Re-arrange ordering of requirejs configuration and setup.

--- app/public/javascripts/main-client.js
+++ app/public/javascripts/main-client.js
@@ -1,9 +1,3 @@
-var config = {
-  'shim': {
-    'lib/moment': { exports: function() { return this.moment; } }
-  }
-}
-
 var getParam = function(a) {
   return (a = location.search.match(RegExp("[?&]" + a + "=([^&]*)(&?)", "i"))) ? a[1] : a
 }
@@ -13,22 +7,24 @@ var dataSource = getParam('data');
 var numberOfDaysToSeedEvents = parseInt(getParam('days')) || 4;
 var updateIntervalInSeconds = parseInt(getParam('interval')) || 20;
 
-if (dataSource == 'fake') {
-  config.map = {
-    '*': { 'data/live_event_stream': 'data/fake_event_stream' }
-  }
+var config = {
+  'shim': { 'lib/moment': { exports: function() { return this.moment; } } }
 }
 
-var locatableEventsFrom = function(allEvents) {
-  return allEvents.filter(function(event) {
-    return event.coordinates;
-  });
+if (dataSource == 'fake') {
+  config.map = { '*': { 'data/live_event_stream': 'data/fake_event_stream' } }
 }
 
 requirejs.config(config);
 
 require(['jquery', 'visualisations/map', 'data/live_event_stream', 'visualisations/ticker', 'visualisations/promoter'],
         function($, map, eventStream, ticker, promoter) {
+
+  var locatableEventsFrom = function(allEvents) {
+    return allEvents.filter(function(event) {
+      return event.coordinates;
+    });
+  };
 
   $(function(){
     eventStream.eventsToDate(numberOfDaysToSeedEvents, function(eventsToDate) {
