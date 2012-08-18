67bff8387f340d655d138847128ec7bb64888686
Mike Gardiner <conversationing@gmail.com>
2012-08-18 16:50:10 +0800

    Correct the lat/lng -> x/y plotting which was, to be honest, totally whacked.

--- app/public/javascripts/visualisations/map.js
+++ app/public/javascripts/visualisations/map.js
@@ -5,13 +5,13 @@ define(
   var width = $("#map").width(), height = $("#map").height();
   var origin_lat = -31.937487, origin_lng = 115.841517
   var end_lat = -31.97317, end_lng = 115.880442;
-  var lat_width = Math.abs(end_lat - origin_lat), lng_height = Math.abs(end_lng - origin_lng);
+  var lat_height = Math.abs(end_lat - origin_lat), lng_width = Math.abs(end_lng - origin_lng);
 
   var to_cartesian = function(coords) {
     // TODO: Adjust for map rotation
     return {
-      x: (Math.abs(end_lat - coords.lat) / lat_width) * width,
-      y: (Math.abs(end_lng - coords.lng) / lng_height) * height
+      x: (Math.abs(coords.lng - origin_lng) / lng_width) * width,
+      y: (Math.abs(coords.lat - origin_lat) / lat_height) * height
     };
   };
 
