062a8965f82ca372fa3e6c10d93fca0d4c7eb619
Ray Grasso <ray.grasso@gmail.com>
2012-08-22 20:52:26 +0800

    A little more renaming
    Dig it.

--- app/public/javascripts/layers/features.js
+++ app/public/javascripts/layers/features.js
@@ -1,24 +1,24 @@
-define(['jquery', 'layers/views/tweet_view'], function($, TweetView) 
+define(['jquery', 'layers/views/tweet'], function($, TweetView)
 {
-  function DetailsLayer(eventSource, canvas) {
+  function FeaturesLayer(eventSource, canvas) {
     this.views = [];
     this.maxConcurrentViews = 3;
     this.layoutManager = new LayoutManager(canvas.width, canvas.height);
 
     var self = this;
     var timerTick = function() {
-      if (self.view.length < self.maxConcurrentViews) 
+      if (self.view.length < self.maxConcurrentViews)
         self.createEventView();
       setTimeout(timerTick, 10000);
     };
     setTimeout(timerTick, 10000);
   };
 
-  DetailsLayer.prototype.createEventView() {
+  FeaturesLayer.prototype.createEventView() {
     viewArea = this.layoutManager.allocateAreaOfDimension(100, 40);
     view = new TweetView(canvas, eventSource.getNextEvent(), viewArea.area);
     var self = this;
-    view.onComplete = function() 
+    view.onComplete = function()
     {
       this.layoutManager.deallocateArea(viewArea.handler);
       self.views.remove(view);
